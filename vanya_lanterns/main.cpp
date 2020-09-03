9596a14e61b8ecf875e579c72e33f36191d3ffd1
Oscar Regis <oregis@anzen.com.mx>
2020-09-02 21:22:20 -0500

    second day

---
+++ vanya_lanterns/main.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp             make_pair
+#define pb             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)((x).size())
+#define fill(x, y)     memset(x, y, sizeof(y))
+#define all(x)         (x).begin(), (x).end()
+#define sci(x)         int x;       scanf("%d", &x);
+#define scii(x, y)     int x, y;    scanf("%d %d", &x, &y);
+#define sciii(x, y, z) int x, y, z; scanf("%d %d %d", &x, &y, &z);
+#define TC(x)          sci(x); while(x --)
+#define eprintf(...)   fprintf(stderr, __VA_ARGS__)
+#define debug(x)       { cerr << #x << " = " << x << endl; }
+#define rep(i, x, y)   for (__typeof(x) i = x; i <= y; i ++)
+#define repi(i, x, y)  for (__typeof(x) i = x; i >= y; i --)
+#define fore(itr, x)   for (__typeof(x.begin()) itr = x.begin(); itr != x.end(); itr ++)
+#define forei(itr, x)  for (__typeof(x.end()) itr = x.end() - 1; itr != x.begin() - 1; itr --)
+typedef long long      ll;
+typedef pair<int, int> ii;
+typedef pair<ii, int>  iii;
+typedef vector<int>    vi;
+typedef vector<ii>     vii;
+typedef vector<iii>    viii;
+const   int            inf = 0;
+const   double         eps = 0;
+const   int            ms  = 0;
+const   int            md  = 0;
+
+const int MAX_N = 1e5 + 5;
+const int MAX_L = 20; // ~ Log N
+const long long MOD = 1e9 + 7;
+const long long INF = 1e9 + 7;
+
+double solve(vector<double> lanterns, double l){
+
+    if(lanterns.size() == 0)
+        return 0;
+
+
+
+    sort(lanterns.begin(), lanterns.end());
+    double ans = max(lanterns[0], l - lanterns[lanterns.size() - 1]);
+
+
+    for(int i = 1; i < lanterns.size(); i++){
+        ans = max((lanterns[i] - lanterns[i-1])/2, ans);
+    }
+    return ans ;
+}
+
+int main() {
+    ll n, l;
+
+    cin >> n;
+    cin >> l;
+    vector<double> lanterns(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> lanterns[i];
+
+    printf("%.10f\n",solve(lanterns, l));
+	return 0;
+}
